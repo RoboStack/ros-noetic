cc30c2192d616bfb21240132061bd75449436c07
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-09-27 22:31:11 +0200

    use python for patching

---
+++ patch/patch_sipconfig.py
@@ -0,0 +1,18 @@
+import os
+
+prefix = os.environ.get('PREFIX')
+
+with open(os.path.join(prefix, 'Lib/site-packages/sipconfig.py')) as fi:
+	lines = fi.readlines()
+	print("\n\nREAD IN SIPCONFIG", lines)
+
+lines[1001:1001] = """
+
+        if sys.platform == "win32":
+            lib += '_conda'
+
+"""
+
+with open(os.path.join(prefix, 'Lib/site-packages/sipconfig.py'), 'w') as fo:
+	print("WRITNIG PATCHE SIPCONFIG")
+	fo.write(''.join(lines))
