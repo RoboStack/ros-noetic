f05ec78dbacb5eb9a2a7299eceea47570d56d52a
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-09-30 11:18:30 +0200

    try once more

--- .scripts/build_linux.sh
+++ .scripts/build_linux.sh
@@ -29,7 +29,7 @@ conda config --add channels robostack
 conda config --set channel_priority strict
 
 conda update conda -c conda-forge
-conda install --yes --quiet boa quetz-client conda-build pip anaconda-client
+conda install --yes --quiet pip conda-build anaconda-client
 
 # install boa from master
 # baaaad
