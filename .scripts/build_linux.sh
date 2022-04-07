801ddfc5b5a9a4cf664bb987e689f63231957340
Tobias Fischer <info@tobiasfischer.info>
2022-04-07 19:25:03 +1000

    Update build_linux.sh

--- .scripts/build_linux.sh
+++ .scripts/build_linux.sh
@@ -34,7 +34,7 @@ conda config --remove channels defaults
 # conda config --set channel_priority strict
 
 mamba update conda --yes --quiet -c conda-forge
-mamba install --yes --quiet pip conda-build anaconda-client mamba boa
+mamba install --yes --quiet pip conda-build anaconda-client mamba boa=0.9
 
 # setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 # export PATH="$HOME/miniconda/bin:$PATH"
