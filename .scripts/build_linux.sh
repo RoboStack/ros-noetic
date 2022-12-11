3924c23174df98362ccb6e17f4a00be6b8d436b1
Tobias Fischer <info@tobiasfischer.info>
2022-12-12 07:22:39 +1000

    mirror ros-humble changes

--- .scripts/build_linux.sh
+++ .scripts/build_linux.sh
@@ -34,7 +34,7 @@ conda config --remove channels defaults
 # conda config --set channel_priority strict
 
 mamba update conda --yes --quiet -c conda-forge
-mamba install --yes --quiet pip conda-build=3.21.8 anaconda-client mamba boa=0.9
+mamba install --yes --quiet pip conda-build anaconda-client mamba boa
 
 # setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 # export PATH="$HOME/miniconda/bin:$PATH"
