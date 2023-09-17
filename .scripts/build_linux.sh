17244eb52e026443af72cda8ff7fce4075fb6f9c
Tobias Fischer <info@tobiasfischer.info>
2023-09-17 11:59:44 +1000

    Update build_linux.sh

--- .scripts/build_linux.sh
+++ .scripts/build_linux.sh
@@ -29,7 +29,6 @@ conda index $CONDA_BLD_PATH
 
 conda config --set remote_max_retries 5
 conda config --add channels conda-forge
-conda config --add channels robostack
 conda config --add channels robostack-staging
 conda config --add channels $CONDA_BLD_PATH
 conda config --remove channels defaults
