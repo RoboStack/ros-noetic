267a03c81cec81df574f564b69f11ae46af3cefe
Tobias Fischer <info@tobiasfischer.info>
2024-12-31 21:17:14 +1000

    Initial commit to move to rattler

--- .scripts/build_win.bat
+++ .scripts/build_win.bat
@@ -1,9 +1,7 @@
 setlocal EnableExtensions EnableDelayedExpansion
-call %CONDA%\condabin\conda_hook.bat
-call %CONDA%\condabin\conda.bat activate base
 
+set CONDA_BLD_PATH=C:\bld
 echo "PATH is %PATH%"
-echo "CONDA_BLD_PATH is %CONDA_BLD_PATH%"
 
 rmdir /Q/S C:\Strawberry\
 rmdir /Q/S "C:\Program Files (x86)\Windows Kits\10\Include\10.0.17763.0\"
@@ -11,25 +9,21 @@ rmdir /Q/S "C:\Program Files (x86)\Windows Kits\10\Include\10.0.17763.0\"
 set "FEEDSTOCK_ROOT=%cd%"
 
 mkdir %CONDA_BLD_PATH%
-call conda index %CONDA_BLD_PATH%
-
-rem call conda config --remove channels defaults
-call conda config --add channels conda-forge
-call conda config --add channels robostack-staging
-call conda config --add channels %CONDA_BLD_PATH%
-:: call conda config --set channel_priority strict
 
 :: Enable long path names on Windows
 reg add HKLM\SYSTEM\CurrentControlSet\Control\FileSystem /v LongPathsEnabled /t REG_DWORD /d 1 /f
 
-:: conda remove --force m2-git
-
 for %%X in (%CURRENT_RECIPES%) do (
     echo "BUILDING RECIPE %%X"
     cd %FEEDSTOCK_ROOT%\recipes\%%X\
-    boa build . -m %FEEDSTOCK_ROOT%\.ci_support\conda_forge_pinnings.yaml -m %FEEDSTOCK_ROOT%\conda_build_config.yaml
+    pixi run -v rattler-build build --recipe %FEEDSTOCK_ROOT%\recipes\%%X\ ^
+        -m %FEEDSTOCK_ROOT%\conda_build_config.yaml ^
+        -c robostack-jazzy -c conda-forge ^
+        --output-dir %CONDA_BLD_PATH%
+
     if errorlevel 1 exit 1
+    rem -m %FEEDSTOCK_ROOT%\.ci_support\conda_forge_pinnings.yaml
 )
 
-anaconda -t %ANACONDA_API_TOKEN% upload "C:\bld\win-64\*.tar.bz2" --force
+pixi run upload "%CONDA_BLD_PATH%\win-64\*.conda" --force
 if errorlevel 1 exit 1
